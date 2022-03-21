50c2eaa571484f77c9e01478d6c4ff4a7f0d6208
Jackson Naufal <Jackson.naufal@mths.ca>
2022-03-21 21:35:30 +0000

    GuessRightNumberProgram

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Jackson Naufal
+# Created on: March 2022
+# This is a constants file for my random guesser program
+
+guess_number = 8
---
+++ guess_right_number.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Jackson Naufal
+# Created on: March 2022
+# This is a guess the random number program
+
+import constants
+
+def main():
+    # This is a random number guesser 
+
+    # input
+    random_number = int(input("Enter your first guess here (0-9): "))
+
+    # process & output
+    if random_number != constants.guess_number:
+        print ("Guess is incorrect")
+    if random_number == constants.guess_number:
+        print ("Guess is correct!")
+
+
+
+
+if __name__ == "__main__":
+    main()
